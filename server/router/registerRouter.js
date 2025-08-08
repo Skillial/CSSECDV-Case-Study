2b6da03465ba2554128198e362337651d6fc4394
alyannacabrera <alyanna_hazel_cabrera@dlsu.edu.ph>
2025-08-08 12:54:11 +0800

    fix: icons

--- server/router/registerRouter.js
+++ server/router/registerRouter.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const router = express.Router();
 const controller = require('./../controller/registerController');
-const { ensureAdmin, ensureAuthenticated } = require('./../middleware/auth');
+const { ensureAdmin, ensureUnauthenticated } = require('./../middleware/auth');
 
-router.get('/register', ensureAuthenticated, controller.page);
-router.post('/register', ensureAuthenticated, controller.register);
+router.get('/register', ensureUnauthenticated, controller.page);
+router.post('/register', ensureUnauthenticated, controller.register);
 router.post('/register/admin', ensureAdmin, controller.registerAdmin);
 router.post('/register/manager', ensureAdmin, controller.registerManager);
 router.post('/employee/assign', ensureAdmin, controller.assignEmployee);
